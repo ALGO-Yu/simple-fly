a8e83bb3cb1175d8120a54114e42b57fb4056b53
taozhengyu <tzyworking@163.com>
2024-12-26 14:13:53 +0800

    Initial Commit

---
+++ sImple-flying/sImple_flyingApp.swift
@@ -0,0 +1,17 @@
+//
+//  sImple_flyingApp.swift
+//  sImple-flying
+//
+//  Created by taozhengyu on 2024/12/26.
+//
+
+import SwiftUI
+
+@main
+struct sImple_flyingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sImple-flyingTests/sImple_flyingTests.swift
@@ -0,0 +1,17 @@
+//
+//  sImple_flyingTests.swift
+//  sImple-flyingTests
+//
+//  Created by taozhengyu on 2024/12/26.
+//
+
+import Testing
+@testable import sImple_flying
+
+struct sImple_flyingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
